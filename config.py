413e4f92fc4e9c4981470b67a3a6fd286caef8da
vostorg <106368478+vostorgmachine@users.noreply.github.com>
2022-06-14 19:24:49 +0000

    Add files via upload

--- config.py
+++ config.py
@@ -15,6 +15,12 @@ mod = "mod4"
 alt = "mod1"
 terminal = guess_terminal()
 
+# Hooks
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~/scripts/first.sh')
+    subprocess.Popen([home])
+
 keys = [
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
@@ -27,26 +33,16 @@ keys = [
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
 
     # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
 
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
+    # Grow windows
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
 
     # Toggle between different layouts as defined below
     Key([mod], "f", lazy.next_layout(), desc="Toggle between layouts"),
@@ -64,13 +60,16 @@ keys = [
     Key([mod], "e", lazy.spawn("emacs")),
     Key([mod], "n", lazy.spawn("librewolf")),
     Key([mod], "m", lazy.spawn("firefox")),
-]
 
-# hooks
-@hook.subscribe.startup_once
-def autostart():
-    home = os.path.expanduser('~/scripts/first.sh')
-    subprocess.Popen([home])
+    # multiple stack panes
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+
+]
 
 groups = [Group(i) for i in "uiop"]
 
@@ -99,7 +98,14 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#43b3ae", "#c6e6e3"], border_width=1, margin = 5, border_focus='#ffffff', border_normal='#000000' ),
+    layout.Columns
+
+    (border_focus_stack=["#43b3ae", "#c6e6e3"],
+    border_width=1,
+    margin = 5,
+    border_focus='#ffffff',
+    border_normal='#000000' ),
+
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
